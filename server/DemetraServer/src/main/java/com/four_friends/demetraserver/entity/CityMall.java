d2be31ddebe55a81c2df55e9320e79c037c22d93
gekko <hellogekko@gmail.com>
2019-09-27 20:08:41 +0300

    add city mall

---
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/entity/CityMall.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.four_friends.demetraserver.entity;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author gekko
+ */
+public class CityMall extends Entity{
+    @DatabaseField(generatedId = true)
+    private long id;
+    
+    @DatabaseField
+    private String name;
+    
+    private List<Restaraunt> restarauntList = new ArrayList<>();
+    
+    @DatabaseField(dataType = DataType.SERIALIZABLE)
+    private Long[] restarauntIds = new Long[]{};
+    
+    @DatabaseField
+    private String city;
+    
+    @DatabaseField
+    private String address;
+    
+    @DatabaseField
+    private long clusterId;
+    
+    @DatabaseField
+    private double longitude;
+    
+    @DatabaseField
+    private double latitude;
+    
+    @DatabaseField
+    private String url;
+    
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long[] getRestarauntIds() {
+        return restarauntIds;
+    }
+
+    public void setRestarauntIds(Long[] restarauntIds) {
+        this.restarauntIds = restarauntIds;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(long clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public CityMall() {
+    }
+    
+
+    public CityMall(String name) {
+        this.name = name;
+    }
+    
+    public boolean setLocation(String location){
+        String[] latlongArray = location.split("[,]");
+        if(latlongArray.length < 2){
+            return false;
+        }
+        try{
+            Double latitude = Double.parseDouble(latlongArray[0]);
+            Double longitude = Double.parseDouble(latlongArray[1]);
+            this.longitude = longitude;
+            this.latitude = latitude;
+            return true;
+        }catch(NumberFormatException ex){
+            System.out.println(ex.toString());
+            return false;
+        }
+    };
+    
+    public void addRestaraunt(Restaraunt restaraunt){
+        List<Long> idsList = new ArrayList<>();
+        Collections.addAll(idsList, restarauntIds);
+        idsList.add(restaraunt.getId());
+        this.restarauntIds = idsList.toArray(new Long[0]);
+        restaraunt.setCity(this.city);
+        restaraunt.setAddress(this.address);
+        
+    }
+    
+    public void updateRestaraultIds(){
+        Long[] updatedRestarauntIds = this.restarauntList
+                .stream()
+                .filter(Objects::nonNull)
+                .map((current_restaraunt) -> {
+                    return current_restaraunt.getId();
+                }).toArray(Long[]::new);
+        setRestarauntIds(updatedRestarauntIds);
+    }
+    
+    private int calClusterId(){
+        return 0;
+    }
+}
