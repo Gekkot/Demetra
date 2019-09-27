c9881537fcb6f2a8c466a9567dfd7da420ae76af
gekko <hellogekko@gmail.com>
2019-09-28 00:11:30 +0300

    add jetty http server and servlets

---
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/http/jetty/servlets/FoodTagsServlet.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.four_friends.demetraserver.http.jetty.servlets;
+
+import com.four_friends.demetraserver.db.test_data_generator.FoodTagGenerator;
+import com.four_friends.demetraserver.entity.FoodTag;
+import com.four_friends.demetraserver.http.jetty.HttpHelper;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author gekko
+ */
+public class FoodTagsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FoodTag> allFoodTags = FoodTagGenerator.getAllFoodTags();
+        HttpHelper.answerEntities(resp, allFoodTags);
+    }
+    
+    
+}
