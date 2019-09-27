1310b2c55ed27b52bcd20b29a4fec1f9a2bc627b
gekko <hellogekko@gmail.com>
2019-09-27 19:53:28 +0300

    remove license header

--- server/DemetraServer/src/main/java/com/four_friends/demetraserver/Main.java
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/Main.java
@@ -1,11 +1,5 @@
 package com.four_friends.demetraserver;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author gekko
