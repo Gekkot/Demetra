ae259f75fa5c5402844557c277ff1c37a3684cf3
gekko <hellogekko@gmail.com>
2019-09-27 19:40:16 +0300

    Add project - pom file(with dep.) and main class

---
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/Main.java
@@ -0,0 +1,17 @@
+package com.four_friends.demetraserver;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gekko
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
