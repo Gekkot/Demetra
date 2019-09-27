360ca3c12ab63695283ac7d687d57581cb69709a
Khafizov Ruslan <piterrus@mail.ru>
2019-09-28 00:16:17 +0300

    Созданы таблицы для контента, началось их заполнение

---
+++ ios-app/DemetraProjectIOS/Models and Classes/Restaurant.swift
@@ -0,0 +1,34 @@
+//
+//  Restaurant.swift
+//  DemetraProjectIOS
+//
+//  Created by Хафизов Руслан on 27/09/2019.
+//  Copyright © 2019 Хафизов Руслан. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Restaurant {
+    var image: UIImage
+    var id: Int
+    var name: String
+    var city: String
+    var address: String
+    var longitude: Double
+    var latitude: Double
+    var cityMollId: Int
+    var ownerId: Int
+    
+    init(image: UIImage, id: Int, name: String, city: String, address: String, longitude: Double, latitude: Double, cityMollId: Int, ownerId: Int){
+        self.image = image
+        self.id = id
+        self.name = name
+        self.city = city
+        self.address = address
+        self.longitude = longitude
+        self.latitude = latitude
+        self.cityMollId = cityMollId
+        self.ownerId = ownerId
+    }
+}
---
+++ ios-app/DemetraProjectIOS/Views and Cells/RestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantCell.swift
+//  DemetraProjectIOS
+//
+//  Created by Хафизов Руслан on 27/09/2019.
+//  Copyright © 2019 Хафизов Руслан. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
