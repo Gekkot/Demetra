4431a4de1bc85ae4c76660d7ea5e822a49987235
gekko <hellogekko@gmail.com>
2019-09-28 14:35:31 +0300

    init db and http at start server

--- server/DemetraServer/src/main/java/com/four_friends/demetraserver/Main.java
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/Main.java
@@ -1,6 +1,11 @@
 package com.four_friends.demetraserver;
 
+import com.four_friends.demetraserver.cache.RestarauntCache;
+import com.four_friends.demetraserver.db.data_provider.IDataProvider;
+import com.four_friends.demetraserver.db.sqlite.SqliteDBConnect;
+import com.four_friends.demetraserver.db.sqlite.SqliteDataProvider;
 import com.four_friends.demetraserver.http.jetty.OwnHttpServer;
+import com.j256.ormlite.support.ConnectionSource;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,7 +16,12 @@ import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
         try {
-            OwnHttpServer ownHttpServer = new OwnHttpServer();
+            SqliteDBConnect sqliteDBConnect = new SqliteDBConnect();
+            sqliteDBConnect.createDB();
+            ConnectionSource connectionSource = sqliteDBConnect.createConnectionSource();
+            IDataProvider dataProvider = new SqliteDataProvider(connectionSource);
+            RestarauntCache restarauntCache = new RestarauntCache(null);
+            OwnHttpServer ownHttpServer = new OwnHttpServer(restarauntCache);
             ownHttpServer.start();
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
