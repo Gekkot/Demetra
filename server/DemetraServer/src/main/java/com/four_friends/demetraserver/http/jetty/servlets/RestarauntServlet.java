d827da4fdc131f97eb3a4bf0d30391ef48394938
gekko <hellogekko@gmail.com>
2019-09-28 11:46:38 +0300

    add stub to restaraunt requests

---
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/http/jetty/servlets/RestarauntServlet.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.four_friends.demetraserver.http.jetty.servlets;
+
+import com.four_friends.demetraserver.db.test_data_generator.CityMallGenerator;
+import com.four_friends.demetraserver.db.test_data_generator.FoodTagGenerator;
+import com.four_friends.demetraserver.db.test_data_generator.RestarauntGenerator;
+import com.four_friends.demetraserver.entity.FoodTag;
+import com.four_friends.demetraserver.http.jetty.HttpHelper;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author gekko
+ */
+public class RestarauntServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+    
+}
