23d51c70b7f073259b2a722e914a2bb40f15ec7a
gekko <hellogekko@gmail.com>
2019-09-29 07:51:29 +0300

    add menu generator query

---
+++ server/DemetraServer/src/main/java/com/four_friends/demetraserver/http/jetty/servlets/MenuAddServler.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.four_friends.demetraserver.http.jetty.servlets;
+
+import com.four_friends.demetraserver.cache.RestarauntCache;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author gekko
+ */
+public class MenuAddServler extends CachingServlets{
+    
+    public MenuAddServler(RestarauntCache restarauntCache) {
+        super(restarauntCache);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("on post");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("on get");
+    }
+    
+    
+    
+    
+}
